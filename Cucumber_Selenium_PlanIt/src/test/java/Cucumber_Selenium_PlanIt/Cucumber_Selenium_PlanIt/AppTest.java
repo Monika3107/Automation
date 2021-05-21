f5f978e9b5091458a28843f22ff55c447ae0bca5
monika <monika@localhost>
2021-05-21 15:11:30 +1000

    Initial Project Setup

---
+++ Cucumber_Selenium_PlanIt/src/test/java/Cucumber_Selenium_PlanIt/Cucumber_Selenium_PlanIt/AppTest.java
@@ -0,0 +1,20 @@
+package Cucumber_Selenium_PlanIt.Cucumber_Selenium_PlanIt;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
