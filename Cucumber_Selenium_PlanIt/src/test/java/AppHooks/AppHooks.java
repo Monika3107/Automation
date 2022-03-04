897eca9422cc5803096ba90eec5be4e998655636
Monika <48540920+Monika3107@users.noreply.github.com>
2022-03-04 14:44:31 +1100

    commit1 (#4)
    * commit1
    
    * commit2
    
    Co-authored-by: Monika Solanki <monikasolanki@Monikas-MacBook-Pro.local>

--- Cucumber_Selenium_PlanIt/src/test/java/AppHooks/AppHooks.java
+++ Cucumber_Selenium_PlanIt/src/test/java/AppHooks/AppHooks.java
@@ -44,7 +44,7 @@ public class AppHooks extends Base {
 	public void tearDown(Scenario scenario) {
 		quitDriver(driver);
 		if(scenario.isFailed()) {
-			//take screenshot
+
 			String screenshotName=scenario.getName().replaceAll("", "_");
 			byte [] sourcePath=((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
 			scenario.attach(sourcePath, "image/png", screenshotName);
